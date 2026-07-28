e054bf2d5a5b35b254026cf818634506b8ed3a76
gabrielesstevam <gsfe1502@gmail.com>
2026-07-28 11:31:35 -0300

    (dashboard-climatico) feat :: abertura e tratamento do modal de fixos

--- dashboard-climatico/js/main.js
+++ dashboard-climatico/js/main.js
@@ -2,22 +2,41 @@
 import * as UI from "./ui.js"
 
 // variables -----------------
-const htmlElements = {
+const htmlElements = { // elementos html
     button_theme:document.getElementById("button-theme"),
+    button_modal_fixed:document.getElementById("button-fixed-modal")
 }
-let systemVariables = {
-    isSwitchingTheme:true,
+let systemVariables = { // variáveis de sistema
+    isClick:true,
+    itens_fixed_qnt:0
 }
-// events --------------------
+//functions --------------------
+function limitActive(def) {
+    if (systemVariables.isClick) {
+        systemVariables.isClick = false
+        def()
+        setTimeout(() => {
+            systemVariables.isClick = true
+        }, 500)
+    }
+}
+// events ----------------------
 window.addEventListener("load", () => { // carregar tema
     UI.themeLoad()
 })
 htmlElements.button_theme.addEventListener("click",() => { // trocar de tema
-    if (systemVariables.isSwitchingTheme) {
-        systemVariables.isSwitchingTheme = false
-        UI.themeSwitch()
-        setTimeout(() => {
-            systemVariables.isSwitchingTheme = true
-        }, 1000)
+    limitActive(UI.themeSwitch)
+})
+htmlElements.button_modal_fixed.addEventListener("click",() => { // abrir/fechar modal de fixos
+    if (systemVariables.itens_fixed_qnt > 0){
+        limitActive(() => UI.modal("fixed"))
+    } else {
+        limitActive(() => UI.warn("Nenhuma cidade foi fixada ainda"))
+        console.log("Nenhuma cidade encontrada :: fixedbutton")
+    }
+})
+document.addEventListener("click",(event) => {
+    if (event.target.id == "button_item_view"){
+        limitActive(() => UI.modal("document"))
     }
 })
--- dashboard-climatico/js/ui.js
+++ dashboard-climatico/js/ui.js
@@ -1,4 +1,27 @@
-// theme -----------------------------------
+const modais = {
+    fixed:document.querySelector(".container-modal-fix"),
+    document:document.querySelector(".container-modal-description")
+}
+const pop_pups = {
+    alertInterface:document.querySelector(".alert"),
+    warnInterface:document.querySelector(".warn")
+}
+// alert ----------------------------------------
+export function alert(mensage){
+    pop_pups.alertInterface.innerHTML = mensage
+    open(pop_pups.alertInterface)
+    setTimeout(() => {
+        close(pop_pups.alertInterface)
+    }, 2000);
+}
+export function warn(mensage){
+    pop_pups.warnInterface.innerHTML = mensage
+    open(pop_pups.warnInterface)
+    setTimeout(() => {
+        close(pop_pups.warnInterface)
+    }, 2000);
+}
+// theme ----------------------------------------
 export function themeLoad(){ // carrega o tema
     const theme = localStorage.getItem("themeStorage") || "light"
     switch (theme){
@@ -11,11 +34,9 @@ export function themeLoad(){ // carrega o tema
             break
         }
     }
-    
 }
 export function themeSwitch(){ // troca o tema
     const theme = localStorage.getItem("themeStorage")
-
     switch (theme){
         case("light"):{
             themeModify("dark")
@@ -37,4 +58,56 @@ function themeModify(theme) { // modifica o tema
     document.querySelector(".wallpaper-video").src = `assets/videos/${theme}-mode.webm`
     localStorage.setItem("themeStorage", theme)
 }
-// -----------------------------------
+// fixed_modal -----------------------------------
+export function modal(modal) {
+    if (modais[modal].classList.contains("closed")){
+        openModal(modais[modal])
+    } else if (modais[modal].classList.contains("opened")){
+        closeModal(modais[modal])
+    }
+}
+function open(UIelement){
+    setTimeout(() => {
+        UIelement.classList.add("emerge")
+        setTimeout(() => {
+            UIelement.classList.remove("closed")
+            UIelement.classList.add("opened")
+            UIelement.classList.remove("emerge")
+        }, 200);
+    }, 200);
+}
+function close(UIelement){
+    setTimeout(() => {
+        UIelement.classList.add("disappear")
+        setTimeout(() => {
+            UIelement.classList.remove("opened")
+            UIelement.classList.add("closed")
+            UIelement.classList.remove("disappear")
+        }, 200);
+    }, 200);
+}
+function closeAll(UIelement){
+    if (UIelement == "modal"){
+        for (const chave in modais){
+            modais[chave].classList.add("disappear")
+        }
+        setTimeout(() => {
+            for (const chave in modais){
+                modais[chave].classList.remove("disappear")
+                modais[chave].classList.remove("opened")
+                modais[chave].classList.add("closed")
+            }
+        }, 200);
+    } else if (UIelement == "pop_up"){
+        for (const chave in pop_pups){
+            pop_pups[chave].classList.add("disappear")
+        }
+        setTimeout(() => {
+            for (const chave in modais){
+                pop_pups[chave].classList.remove("disappear")
+                pop_pups[chave].classList.remove("opened")
+                pop_pups[chave].classList.add("closed")
+            }
+        }, 200);
+    }
+}    
